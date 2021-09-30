709f56a79353472d0b16ab497162825daab5b112
Jonathan Ramos <jonathaniramos360@gmail.com>
2021-09-30 10:33:17 -0400

    (Updated) Complete Lab - Jonathan, Sebastian, Alex

--- .github/lab_3.py
+++ .github/lab_3.py
@@ -10,24 +10,35 @@ import math
 weight = float(input("Enter the weight of package in kg: "))
 distance = float(input("Enter the distance the package needs to be shipped in miles: "))
 
-# Determine rate for shipping package based on weight
+# Determine rate for shipping package based on weight and Validate Input
 if weight <= 2 and weight > 0:
     rate = 1.10 / 500
+    is_valid = True
 elif weight > 2 and weight <= 6:
     rate = 2.20 / 500
+    is_valid = True
 elif weight > 6 and weight <= 10:
     rate = 3.70 / 500
+    is_valid = True
 elif weight > 10 and weight <= 20:
     rate = 4.8 / 500
-else:
-    print("Weight Exceeds shipping limit! (Please enter between 0kg - 20kg)")
+    is_valid = True
+elif weight > 20:
+    print("\nWeight Exceeds shipping limit! (Please enter between 0kg - 20kg)")
+    is_valid = False
 
-# Determine if distance is valid
-if distance >= 10 and distance <= 3000:
-    shipping_total = rate * distance
-    math.ceil(shipping_total)
-    print("\nYour total for shipping this package is: $" + str(shipping_total) + " Dollars ")
+
+
+# Determine if distance is valid and total shipping calcuation
+if is_valid == True:
+    if distance >= 10 and distance <= 3000:
+        # Total Shipping Calculation
+        shipping_total = rate * distance
+        math.ceil(shipping_total)
+        print("\nYour total for shipping this package is: $" + str(shipping_total) + " Dollars ")
+    else:
+        print("\nYour package cannot be shipped! Does not meet distance requirements.")
 else:
     print("\nYour package cannot be shipped!")
 
-print("\nThanks for using our code")
+print("Thanks for using our code!")
